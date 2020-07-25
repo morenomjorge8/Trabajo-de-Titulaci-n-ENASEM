13a503b1007ad795870ea4ab9d44a0671d615ef7
Jorge Luis Moreno Monreal <57849034+morenomjorge8@users.noreply.github.com>
2020-07-25 02:02:04 -0700

    Update Tratamiento Inicial.R

--- Tratamiento Inicial.R
+++ Tratamiento Inicial.R
@@ -1,34 +1,34 @@
 
 #Script de tratamiento de datos de Enasem 2015
 
-#Carga de paquetería a utilizar
+#Carga de paqueterÃ­a a utilizar
 if (!require(pacman)) install.packages("pacman") 
 library(pacman)
 p_load("readstata13" , #Lector STATA .DTA
-       "tidyverse",    #ManipulaciÃ³n de datos
+       "tidyverse",    #ManipulaciÃƒÂ³n de datos
        "survey",       #Marco Estadistico
-       "stargazer",    #PresentaciÃ³n de tablas
+       "stargazer",    #PresentaciÃƒÂ³n de tablas
        "readxl",       #Excel  
-       "corrplot")     #Visualización de Correlaciones 
+       "corrplot")     #VisualizaciÃ³n de Correlaciones 
 
-setwd("C:/Users/jorge.monreal/Desktop/ENASEM") # MODIFICAR! UBICACIÓN DE LOS DATOS
+setwd("C:/Users/jorge.monreal/Desktop/ENASEM") # MODIFICAR! UBICACIÃ“N DE LOS DATOS
 
 #Carga de Data Frame: df
 df <- read.dta13("dta/2015/sect_a_c_d_e_pc_f_h_i_2015.dta")
 
-#En este script se pretende extraer las  siguienter varaibles de interés 
-#para el trabajo de invertigación: 
+#En este script se pretende extraer las  siguienter varaibles de interÃ©s 
+#para el trabajo de invertigaciÃ³n: 
 
 # Variables de salud
-# SEXO, EDAD, ESCOLARIDAD, RURAL, AltaMigración
-# Número de hijos
-# IMC, DAIBETES, HIPERTENSIÓN, Tabaquismo
-# Ingreso, Pensión, Transferencias
+# SEXO, EDAD, ESCOLARIDAD, RURAL, AltaMigraciÃ³n
+# NÃºmero de hijos
+# IMC, DAIBETES, HIPERTENSIÃ“N, Tabaquismo
+# Ingreso, PensiÃ³n, Transferencias
 
 #Merge y grardar .csv
 
 ### Variables de salud
-##Auto Evaluación de la Salud #aes#
+##Auto EvaluaciÃ³n de la Salud #aes#
 df <- df %>% 
   mutate(
     aes= if_else(as.integer(c1_15)<=3,0,if_else(as.integer(c1_15)<=5,1,NaN))
@@ -53,7 +53,7 @@ df <- df %>%
     h6= if_else(h6_15=="2.No",0,1), #Subir varios pisos de escaleras
     h8= if_else(h8_15=="2.No",0,1), #Arodillarse, agacharse o ponerse de cunclillas
     h9= if_else(h9_15=="2.No",0,1), #Extender los brazos arriba de los hombros
-    h11= if_else(h11_15=="2.No",0,1), #Levantar o transportar objetos  que pesan más de 5 kilos
+    h11= if_else(h11_15=="2.No",0,1), #Levantar o transportar objetos  que pesan mÃ¡s de 5 kilos
     h12= if_else(h12_15=="2.No",0,1), #Rocojer una moneda de un peso de la mesa
     laf= h1+h5+h6+h8+h9+h11+h12
   )
@@ -80,7 +80,7 @@ summary(df$h15a_15)
 df <- df %>% 
   mutate(
     h15= if_else(h15a_15=="2.No",0,1), #Caminar
-    h16= if_else(h16a_15=="2.No",0,1), #Bañarse
+    h16= if_else(h16a_15=="2.No",0,1), #BaÃ±arse
     h17= if_else(h17a_15=="2.No",0,1), #Comer
     h18= if_else(h18a_15=="2.No",0,1), #Ir a la cama
     h19= if_else(h19a_15=="2.No",0,1), #Usar el excusado
@@ -104,7 +104,7 @@ df %>%
   )
 
 
-###SEXO, EDAD, ESCOLARIDAD, RURAL, AltaMigración
+###SEXO, EDAD, ESCOLARIDAD, RURAL, AltaMigraciÃ³n
 df$age_15[df$age_15 == 999] <- NA
 df$age_15[df$age_15 == 888] <- NA
 
@@ -116,7 +116,7 @@ df <- df %>%
     escolaridad= yrschool,
     escolaridad2= yrschool * yrschool,
     rural= if_else(tam_loc_15=="1.Population = 100,000+",0,1),
-    altamigración=eam_15
+    altamigraciÃ³n=eam_15
   )
 
 #Revisamos las variables
@@ -129,15 +129,15 @@ df %>%
     escolaridad=mean(escolaridad,na.rm=TRUE),
     escolaridad2=mean(escolaridad2,na.rm=TRUE),
     rural=mean(rural,na.rm=TRUE),
-    altamigración=mean(altamigración,na.rm=TRUE)
+    altamigraciÃ³n=mean(altamigraciÃ³n,na.rm=TRUE)
   )
 
 
-###Número de hijos
+###NÃºmero de hijos
 summary(as.factor(df$a7_2_15))
 df$a7_2_15[df$a7_2_15 == 99] <- NA
 df$a7_2_15[df$a7_2_15 == 88] <- NA
-summary(df$a7_2_15) #¿Cuántos hijos /hijas que nacieron vivos ha tenido usted?
+summary(df$a7_2_15) #Â¿CuÃ¡ntos hijos /hijas que nacieron vivos ha tenido usted?
 
 
 df <- df %>% 
@@ -153,16 +153,16 @@ df %>%
     Promedio_de_hijos=mean(n_hijos, na.rm = TRUE)
   )
 
-#IMC, DAIBETES, HIPERTENSIÓN, Tabaquismo
+#IMC, DAIBETES, HIPERTENSIÃ“N, Tabaquismo
 ##IMC (peso/altura^2)
 summary(as.factor(df$c66_15))
 df$c66_15[df$c66_15 == 999] <- NA
 df$c66_15[df$c66_15 == 888] <- NA
 summary(as.factor(df$c66_15))
-summary(df$c66_15) #¿Como cuántos kilos pesa usted ahora?
+summary(df$c66_15) #Â¿Como cuÃ¡ntos kilos pesa usted ahora?
 
-summary(as.factor(df$c67_1_15)) #¿Como cuánto mide usted sin zapatos? - Metros
-summary(as.factor(df$c67_2_15)) #¿Como cuánto mide usted sin zapatos? - Centímetros
+summary(as.factor(df$c67_1_15)) #Â¿Como cuÃ¡nto mide usted sin zapatos? â€“ Metros
+summary(as.factor(df$c67_2_15)) #Â¿Como cuÃ¡nto mide usted sin zapatos? â€“ CentÃ­metros
 
 df$c67_1_15[df$c67_1_15== 999] <- NA
 df$c67_1_15[df$c67_1_15 == 888] <- NA
@@ -171,20 +171,20 @@ df$c67_2_15[df$c67_2_15 == 888] <- NA
 
 
 df<-df %>% mutate(
-  talla_cm_total=  (100) + c67_2_15, #Centímetros totales
+  talla_cm_total=  (100) + c67_2_15, #CentÃ­metros totales
   kilos= c66_15,
   imc= kilos/(talla_cm_total*talla_cm_total/10000),
   obes= if_else(imc >= 30,1,0)
 )
 
-#DAIBETES, HIPERTENSIÓN
-summary(df$c6_15) #¿Alguna vez le ha dicho un doctor o personal
-                  # médico que usted tiene diabetes?
+#DAIBETES, HIPERTENSIÃ“N
+summary(df$c6_15) #Â¿Alguna vez le ha dicho un doctor o personal
+                  # mÃ©dico que usted tiene diabetes?
 df$c6_15[df$c6_15== "9.DK"] <- NA
 df$c6_15[df$c6_15 == "8.RF"] <- NA
 
-summary(df$c4_15) #¿Alguna vez le ha dicho un doctor o personal
-                  # médico que usted tiene hipertensión o presión alta?
+summary(df$c4_15) #Â¿Alguna vez le ha dicho un doctor o personal
+                  # mÃ©dico que usted tiene hipertensiÃ³n o presiÃ³n alta?
 df$c4_15[df$c4_15== "9.DK"] <- NA
 df$c4_15[df$c4_15 == "8.RF"] <- NA
 
@@ -194,7 +194,7 @@ df<-df %>% mutate(
 )
 
 #Tabaquismo
-#¿Fuma cigarros actualmente? - c54_15
+#Â¿Fuma cigarros actualmente? - c54_15
 summary(df$c54_15)
 df$c54_15[is.na(df$c54_15)] <- "2.No"
 
@@ -247,11 +247,19 @@ df %>%
 
 #----
 
-# Ingreso, Gasto, Pensión, Transferencias
+# Ingreso, Gasto, PensiÃ³n, Transferencias
 df_income <- read.dta13("dta/2015/Income_2015.dta")
 
 df$subhog_15 <- as.numeric(df$subhog_15)
 df <- left_join(df,df_income,by = c("cunicah", "subhog_15", "np"))
+
+glimpse(df)
+
+df <- df[c("cunicah","np","subhog_15","factori_15","aes","laf","lavd",
+           "tabaquismo","hipertens","diabetes","obes","imc","kilos","n_hijos",
+           "altamigraciÃ³n","rural","escolaridad2","escolaridad","sexo_mujer",
+           "edad2","edad","IGNORAR",
+           "h1","h5","h6","h8","h9","h11","h12","h15","h16","h17","h18","h19")]
 
 
 write.csv(df,"resultados/DataFrame_Variables_brutas.csv")
